a534800d18e8bdb4d628dc43065f25559b28e052
ST10444951 <ST10444951@vcconnect.edu.za>
2024-04-05 11:46:25 +0200

    History App

--- app/src/main/java/za/ac/iie/st1044951Ass1/MainActivity.kt
+++ app/src/main/java/za/ac/iie/st1044951Ass1/MainActivity.kt
@@ -54,15 +54,15 @@ class MainActivity : AppCompatActivity() {
                     // to check when the user has entered an age
                     val result = when (userAge) {
                         26 -> "Sarah Baartman, She was viewed by Westerners as the Hottentot Venus, a representation of their repressed passions and fears, and as seductive and savage,26."
-                        30 -> "Steve Biko,30 "
-                        47 -> "Shona Ferguson,47"
-                        50 -> "Micheal Jackson,50"
-                        65 -> "Patrick Shai,65"
-                        69 -> "Albert John Luthuli,69"
-                        74 -> "Muhammad Ali,74"
-                        81 -> "Winnie Mandela,81"
-                        90 -> "Desmount Tuto,90"
-                        96 -> "Queen Elizabeth,96"
+                        30 -> "Steve Biko,Global martyr for Black nationalism in South Africa,30 "
+                        47 -> "Shona Ferguson,Shona Ferguson was an influential figure in the film industry,47"
+                        50 -> "Micheal Jackson,American singer, composer, and dancer who was the most well-liked performer worldwide in the early and middle 1980s,50"
+                        65 -> "Patrick Shai,Shai, a South African soap opera icon known for his paternal roles in beloved TV series ,65"
+                        69 -> "Albert John Luthuli,Luthuli was a Zulu chief, educator, and spiritual guide who served as president of the African National Congress in South Africa ,69"
+                        74 -> "Muhammad Ali,Muhammad Ali was a social activist and professional boxer from America,74"
+                        81 -> "Winnie Mandela,, South Africans social worker and activist considered by many Black South Africans to be the “Mother of the Nation,81"
+                        90 -> "Desmount Tuto,south African Anglican cleric and theologian, who in 1984 received the Nobel Prize for Peace for his role in the opposition to apartheid in South Africa,90"
+                        96 -> "Queen Elizabeth,The queen of the United Kingdom of Great Britain and Northern Ireland ,96"
                         else -> "Nobody known to me died at this age"
 
 
